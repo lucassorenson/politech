3f89f31d9a88e588a92fcf2047131677c3e439e3
Lucas <lucas@osetfoundation.org>
2019-11-22 08:33:20 -0500

    create store and render app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { App } from './app';
+import rootReducer from './reducers'
+
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(rootReducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
